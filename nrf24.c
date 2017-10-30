28a38bf277cdd806c48687f8adc974880321858a
Keyhan Asadi <keyhana97@gmail.com>
2017-10-30 19:25:08 +0330

    Initial Commit

---
+++ nrf24.c
@@ -0,0 +1,13 @@
+#include "nrf24.h"
+
+unsigned char status;
+
+void write_register(unsigned char command, unsigned char* data, unsigned int length)
+{
+
+}
+
+void read_register(unsigned char command, unsigned char* data, unsigned int length)
+{
+
+}
